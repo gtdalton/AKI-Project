dfe6ecea072716519dc77ea92d592a398e97cbe7
Tom Dalton <36134580+gtdalton@users.noreply.github.com>
2019-10-25 15:54:59 +0100

    add inclusion criteria file

---
+++ methods_2_inclusion_criteria.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from methods_1_data_cleaning import set_data_set
+
+
+
+def inclusion_criteria(data_set, minimum_age=18,min_num_creatinine_readings=2):
+    input_folder = data_set + "cleaned_data/"
+    episodes = pd.read_csv(input_folder + 'episodes.csv', parse_dates=['admission_date'])
+
+    # Remove under 18
+    num_episodes = episodes.shape[0]
+    episodes = episodes[episodes['admission_age']>=minimum_age]
+    print('%s episodes removed as patient under 18' %(num_episodes - episodes.shape[0]))
+
+    # Remove episodes with less than two creatinine readings
+    num_episodes = episodes.shape[0]
+    creatinine = pd.read_csv(input_folder + 'creatinine.csv', parse_dates = ['chartTime'])
+    creatinine_value_counts = creatinine['episode'].value_counts()
+    included_episodes = list(creatinine_value_counts[creatinine_value_counts >= min_num_creatinine_readings].index)
+    episodes = episodes[episodes['episode'].isin(included_episodes)]
+    print ('%s episodes removed as <2 creatinine readings' %(num_episodes - episodes.shape[0]))
+
+    # remove episodes with a recording of chronic renal replacement therapy
+    if 'chronic_renal_replacement_therapy' in episodes.columns:
+        num_episodes = episodes.shape[0]
+        episodes = episodes[episodes['chronic_renal_replacement_therapy']!= 1]
+        print('%s episodes removed due to chronic renal replacement therapy' %(num_episodes - episodes.shape[0]))
+
+    episodes.to_csv(data_set + 'included_episodes.csv', index=False)
+
+def main():
+    data_set = set_data_set()
+    inclusion_criteria()
+
+if __name__=='__main__':
+    main()
